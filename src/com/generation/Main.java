1b1e5dd633d3618221a425aa46761aabee0aa565
Abigail Romero <romerot.abigail@gmail.com>
2021-10-20 20:15:28 -0500

    excepciones, try catch y finally

--- src/com/generation/Main.java
+++ src/com/generation/Main.java
@@ -5,8 +5,36 @@ import com.generation.ejercicio.Person;
 import com.generation.paquete2.Animal;
 
 public class Main {
+    public static int numerador = 10;  //estos son atributos
+    public static int denominador = 0;
+    public static float division;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //este es un método
+        //la diferencia entre los errores y las excepciones es que
+        //los errores no dependen de nuestro codigo, las excepciones si
+        //las excepciones engloba todas las subclases
+        //es posible anidar los catch
+        //si se anidan tiene que ir de lo especifico a lo general
+        //por ejemplo, primero nullpointer exception y despues exception
+        System.out.println("antes de la division");
+        try { //solo se puede atrapar 1 excepcion
+            division = numerador/denominador;
+        }catch (NullPointerException ex){
+            System.out.println("Nullpointer");
+            System.out.println(ex.getMessage());
+
+        } catch (Exception ex){
+            //en los parentesis sed indica que vamos a "capturar" una excepcion
+            //se guarda en la variable ex
+            System.out.println("Exception");
+            System.out.println( ex.getMessage()); //se rescata porque fallo el codigo
+        } finally {
+            //este bloque siempre se ejecuta
+            //es opcional, puede estar o no
+            System.out.println("soy finally :v");
+        }
+
+        System.out.println("despues de la division");
 	// write your code here
 //        Persona persona = new Persona();
 //        persona.nombre = "Abby"; //indica problemas de acceso si los atributos estan definidos coo privados
@@ -14,10 +42,11 @@ public class Main {
 //        Animal animal = new Animal(); //se puede crear objeto pero no acceder a sus atributoso definirlos
 //        animal.nombre = "Pájaro"; //no hay acceso
 
-        CuentaBancaria cuenta = new CuentaBancaria();
-        cuenta.setNombre("Abby");
-        cuenta.setClave("ABC-120896");
-        cuenta.setSaldo(1000000000);
+//        CuentaBancaria cuenta = new CuentaBancaria();
+//        cuenta.setNombre("Abby");
+//        cuenta.setClave("ABC-120896");
+//        cuenta.setSaldo(1000000000);
+
 //        cuenta.mostrarNombre();
 //        cuenta.mostrarSaldo();
 
@@ -28,16 +57,17 @@ public class Main {
 //        Person person= new Person("Santiago", 33, "102045455");
 //        System.out.println( "Name: " + person.name);
 //        System.out.println( "Age: "+ person.age);
-//        System.out.println( "Id: " +  person.id);
-        Person person = new Person();
-        person.setName("Santiago");
-        person.setAge(33);
-        person.setId("102045455");
-
-        System.out.println("Name: "+(person.getName())); //opcion 1
-        System.out.println("Age: "+(person.getAge()));
-        System.out.println("Id: "+(person.getId()));
+////        System.out.println( "Id: " +  person.id);
+//        Person person = new Person();
+//        person.setName("Santiago");
+//        person.setAge(33);
+//        person.setId("102045455");
+//
+//        System.out.println("Name: "+(person.getName())); //opcion 1
+//        System.out.println("Age: "+(person.getAge()));
+//        System.out.println("Id: "+(person.getId()));
         //opcion 2 de imprimir
         //person.mostrarInfo();
+
     }
 }
